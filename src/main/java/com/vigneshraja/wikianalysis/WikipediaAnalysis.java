fa0eed1888d7290ed867ce031d5de83dd9660097
Vignesh Raja <vignesh@optimizely.com>
2018-08-30 22:42:45 -0700

    Refactor WikipediaAnalysis class to remove deprecated classes

--- src/main/java/com/vigneshraja/wikianalysis/WikipediaAnalysis.java
+++ src/main/java/com/vigneshraja/wikianalysis/WikipediaAnalysis.java
@@ -1,10 +1,6 @@
 package com.vigneshraja.wikianalysis;
 
-import org.apache.flink.api.common.functions.FoldFunction;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -18,37 +14,39 @@ import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
  */
 public class WikipediaAnalysis {
 
+    private static final String TUPLE_OUTPUT_TEMPLATE = "(%s, %d)";
+
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment see = StreamExecutionEnvironment.getExecutionEnvironment();
 
         DataStream<WikipediaEditEvent> edits = see.addSource(new WikipediaEditsSource());
 
-        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits
-            .keyBy(new KeySelector<WikipediaEditEvent, String>() {
-                @Override
-                public String getKey(WikipediaEditEvent event) {
-                    return event.getUser();
-                }
-            });
+        KeyedStream<WikipediaEditEvent, String> keyedEdits = edits.keyBy(WikipediaEditEvent::getUser);
 
-        DataStream<Tuple2<String, Long>> result = keyedEdits
+        DataStream<WikipediaEditEvent> result = keyedEdits
             .timeWindow(Time.seconds(5))
-            .fold(new Tuple2<>("", 0L), new FoldFunction<WikipediaEditEvent, Tuple2<String, Long>>() {
-                @Override
-                public Tuple2<String, Long> fold(Tuple2<String, Long> acc, WikipediaEditEvent event) {
-                    acc.f0 = event.getUser();
-                    acc.f1 += event.getByteDiff();
-                    return acc;
-                }
-            });
+            // using reduce as a hack, can't use "sum" since the WikipediaEditEvent doesn't have a default constructor
+            // defined
+            .reduce((WikipediaEditEvent e1, WikipediaEditEvent e2) -> new WikipediaEditEvent(
+                e1.getTimestamp(),
+                e1.getChannel(),
+                e1.getTitle(),
+                e1.getDiffUrl(),
+                e1.getUser(),
+                e1.getByteDiff() + e2.getByteDiff(),
+                e1.getSummary(),
+                e1.isMinor(),
+                e1.isNew(),
+                e1.isUnpatrolled(),
+                e1.isBotEdit(),
+                e1.isSpecial(),
+                e1.isTalk())
+            );
 
         // Sink to Kafka
-        result.map(new MapFunction<Tuple2<String,Long>, String>() {
-            @Override
-            public String map(Tuple2<String, Long> tuple) {
-                return tuple.toString();
-            }
-        }).addSink(new FlinkKafkaProducer011<>("localhost:9092", "wiki-result", new SimpleStringSchema()));
+        result
+            .map((WikipediaEditEvent event) -> String.format(TUPLE_OUTPUT_TEMPLATE, event.getUser(), event.getByteDiff()))
+            .addSink(new FlinkKafkaProducer011<>("localhost:9092", "wiki-result", new SimpleStringSchema()));
 
         see.execute();
     }
